96ae3bb217bc8e1591d163d38227fed713127585
Julie Engel <julie.engel@mail.com>
2023-03-29 20:02:36 +0200

    add section about looping

--- 02_controlflow.kt
+++ 02_controlflow.kt
@@ -45,4 +45,52 @@ fun main() {
     println(whenAssign(MyClass()))
 }
 
+// part 2: loops
+// kotlin supports all types of loops: for, while, do-while
 
+fun main() {
+    
+    // for
+    
+    val cakes = listOf("carrot", "cheese", "chocolate")
+
+    for (cake in cakes) {
+        println("Yummy, it's a $cake cake!")
+    }
+
+    // while and do-while
+    
+    fun eatACake() = println("Eat a Cake")
+	fun bakeACake() = println("Bake a Cake")
+    
+    var cakesEaten = 0
+    var cakesBaked = 0
+    
+    while (cakesEaten < 5) {
+        eatACake()
+        cakesEaten++
+    }
+    
+    do {
+        bakeACake()
+        cakesBaked++
+    } while (cakesBaked < cakesEaten)
+}
+
+// you can also use iterators:
+
+class Animal(val name: String)
+
+class Zoo(val animals: List<Animal>) {
+    
+    operator fun iterator(): Iterator<Animal> {
+		return animals.iterator()
+    }
+}
+
+fun main() {
+    val zoo = Zoo(listOf(Animal("Zebra"), Animal("Tiger")))
+    for (animal in zoo) {
+        println("watch out, it's a ${animal.name}")
+    }
+}
