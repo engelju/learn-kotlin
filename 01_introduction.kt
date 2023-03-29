f4647e188d7abd7648cc9bb7890746c7195ae1f9
Julie Engel <julie.engel@mail.com>
2023-03-29 19:32:00 +0200

    add section about generics

--- 01_introduction.kt
+++ 01_introduction.kt
@@ -160,6 +160,35 @@ fun main() {
     println(contact.email)
 }
 
+// part 6: generics
 
+// generics encapsulate logic that is independent regarding any particular type,
+// like for example the logic inside a List<T> is independent regardless what type T is.
+
+class MutableStack<E>(vararg items: E) {
+    private val elements = items.toMutableList()
+    
+    fun push(element: E) {
+        elements.add(element)
+    }
+    
+    fun peek(): E {
+        return elements.last()
+    }
+    
+    // don't forget, we can also shorten function definitions:
+    fun pop(): E = elements.removeAt(elements.size - 1)
+	fun isEmpty() = elements.isEmpty()
+    fun size() = elements.size
+    
+    override fun toString() = "MutableStack(${elements.joinToString()})"
+}
+
+fun <E> mutableStackOf(vararg elements: E) = MutableStack(*elements)
+
+fun main() {
+  val stack = mutableStackOf(0.62, 3.14, 2.7)
+  println(stack)
+}
 
 
